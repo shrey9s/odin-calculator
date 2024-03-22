afc391d8d5fe0935822ff0f6b6d0cff5104f4f21
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-22 21:20:05 +1300

    Add event listener to equals button

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ function divide(a, b) {
 }
 
 function operate(op, a, b) {
+    a = +a;
+    b = +b;
     let result;
     switch (op) {
         case "+":
@@ -26,10 +28,10 @@ function operate(op, a, b) {
         case "-":
             result = subtract(a, b);
             break;
-        case "*":
+        case "×":
             result = multiply(a, b);
             break;
-        case "/":
+        case "÷":
             result = divide(a, b);
             break;
     }
@@ -54,16 +56,16 @@ function processInput(input) {
             num1 = input;
         }
         else { // other inputs already in display
-            const lastValue = (lastChild.textContent).trim();
-            const previousSibling = lastChild.previousSibling;
+            const lastValue = (lastChild.textContent).trim(); // the last input (before current one)
+            const previousSibling = lastChild.previousSibling; // second-to-last input
             if (opArr.includes(lastValue) && previousSibling && !opArr.includes(previousSibling.textContent.trim())) { // the last input is operator AND NOT (-); user is inputting second number
                 num2 = input;
             }
-            else { // the last input is number, (-), or (.); user is inputting 2+ digit, negative, or decimal point number
+            else { // the last input is number, (-), or (.); user is inputting 2+ digit, negative, or decimal point number (for first or second number)
                 if (num2 === undefined) { // user has not begun inputting second number - i.e., user is still inputting first number (it is a 2+ digit/negative/decimal number)
                     num1 += input;
                 }
-                else { // user has already begun inputting second number
+                else { // user has already begun inputting second number (it is a 2+ digit/negative/decimal number)
                     num2 += input;
                 }
             }
@@ -71,6 +73,7 @@ function processInput(input) {
     }
 }
 // If lastValue = "-" but this refers to the negative sign (not minus):
+// The negative sign is the first part of num1 and num2
 // If user has input the negative sign for num1, then there should be NO previous sibling (i.e., "-" is the first input in display)
 // If user has input the negative sign for num2, then there should be a previous sibling AND that previous sibling should be an operator (not a number)
 
@@ -85,6 +88,13 @@ valueButtons.forEach((btn) => {
         processInput(value);
         displayValue(value);
     });
+});
+
+const equalsBtn = document.querySelector(".equals-btn");
+equalsBtn.addEventListener("click", () => {
+    display.textContent = "";
+    const answer = (operate(operator, num1, num2)).toFixed(4);
+    displayValue(answer);
 });
 
 const clearBtn = document.querySelector(".clear-btn");

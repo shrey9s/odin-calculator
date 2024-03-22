9d0dd5036f2f29b066d560e0f6b1306c9d282899
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-22 14:11:59 +1300

    Modify displayValue and implement processInput function
    processInput is currently able to process single-digit, multiple digit,
    decimal point and negative number inputs, and assign them to the num1
    and num2 variables accordingly, as well as assign the operator inputted
    to the variable operator.

--- script.js
+++ script.js
@@ -37,19 +37,53 @@ function operate(op, a, b) {
 }
 
 function displayValue(val) {
-    const textNode = document.createTextNode(val + " ");
+    const isOperator = opArr.includes(val);    
+    if (val === "(-)") val = "-";
+    const textNode = document.createTextNode(isOperator ? ` ${val} `: val);
     display.appendChild(textNode);
 }
 
+function processInput(input) {
+    if (opArr.includes(input)) { // input is operator
+        operator = input;
+    }
+    else { // input is number, negative sign (-), or decimal point (.)
+        if (input === "(-)") input = "-";
+        const lastChild = display.lastChild;
+        if (!lastChild) { // input is the very first input
+            num1 = input;
+        }
+        else { // other inputs already in display
+            const lastValue = (lastChild.textContent).trim();
+            const previousSibling = lastChild.previousSibling;
+            if (opArr.includes(lastValue) && previousSibling && !opArr.includes(previousSibling.textContent.trim())) { // the last input is operator AND NOT (-); user is inputting second number
+                num2 = input;
+            }
+            else { // the last input is number, (-), or (.); user is inputting 2+ digit, negative, or decimal point number
+                if (num2 === undefined) { // user has not begun inputting second number - i.e., user is still inputting first number (it is a 2+ digit/negative/decimal number)
+                    num1 += input;
+                }
+                else { // user has already begun inputting second number
+                    num2 += input;
+                }
+            }
+        }
+    }
+}
+// If lastValue = "-" but this refers to the negative sign (not minus):
+// If user has input the negative sign for num1, then there should be NO previous sibling (i.e., "-" is the first input in display)
+// If user has input the negative sign for num2, then there should be a previous sibling AND that previous sibling should be an operator (not a number)
+
 let num1, operator, num2;
+const opArr = ["+", "-", "×", "÷"];
 const display = document.querySelector(".display-screen");
 
 const valueButtons = document.querySelectorAll(".value");
 valueButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
-        let val = btn.textContent;
-        if (val === "(-)") val = "-";
-        displayValue(val);
+        let value = btn.textContent;
+        processInput(value);
+        displayValue(value);
     });
 });
 
@@ -57,6 +91,11 @@ const clearBtn = document.querySelector(".clear-btn");
 clearBtn.addEventListener("click", () => {
     display.textContent = "";
 });
+
+/*const deleteBtn = document.querySelector(".delete-btn");
+deleteBtn.addEventListener("click", () => {
+    display.removeChild(display.lastChild);
+});*/
 
 
 

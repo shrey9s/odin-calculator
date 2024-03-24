c3a7dceb5208e4397a11319c1efef115504190d4
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-24 15:34:20 +1300

    Replace processInput and add error checks to equals button event listener

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b === 0) {
-        return "ERROR: Division by zero";
+        return "Math ERROR";
     }
     return a / b;
 }
@@ -41,43 +41,16 @@ function operate(op, a, b) {
 }
 
 function displayValue(val) {
-    const isOperator = opArr.includes(val);    
+    const isOperator = opArr.includes(val);
     if (val === "(-)") val = "-";
-    const textNode = document.createTextNode(isOperator ? ` ${val} `: val);
+    const textNode = document.createTextNode(isOperator ? ` ${val} ` : val);
     display.appendChild(textNode);
 }
 
-function processInput(input) {
-    if (opArr.includes(input)) { // input is operator
-        operator = input;
-    }
-    else { // input is number, negative sign (-), or decimal point (.)
-        if (input === "(-)") input = "-";
-        const lastChild = display.lastChild;
-        if (!lastChild) { // input is the very first input
-            num1 = input;
-        }
-        else { // other inputs already in display
-            const lastValue = (lastChild.textContent).trim(); // the last input (before current one)
-            const previousSibling = lastChild.previousSibling; // second-to-last input
-            if (opArr.includes(lastValue) && previousSibling && !opArr.includes(previousSibling.textContent.trim())) { // the last input is operator AND NOT (-); user is inputting second number
-                num2 = input;
-            }
-            else { // the last input is number, (-), or (.); user is inputting 2+ digit, negative, or decimal point number (for first or second number)
-                if (num2 === undefined) { // user has not begun inputting second number - i.e., user is still inputting first number (it is a 2+ digit/negative/decimal number)
-                    num1 += input;
-                }
-                else { // user has already begun inputting second number (it is a 2+ digit/negative/decimal number)
-                    num2 += input;
-                }
-            }
-        }
-    }
+function setValues(eqn) {
+    const eqnParts = eqn.split(" ");
+    [num1, operator, num2] = [eqnParts[0], eqnParts[1], eqnParts[2]]
 }
-// If lastValue = "-" but this refers to the negative sign (not minus):
-// The negative sign is the very first part of num1 and num2
-// If user has input the negative sign for num1, then there should be NO previous sibling (i.e., "-" is the first input in display)
-// If user has input the negative sign for num2, then there should be a previous sibling AND that previous sibling should be an operator (not a number)
 
 let num1, num2, operator;
 const opArr = ["+", "-", "×", "÷"];
@@ -86,20 +59,41 @@ const display = document.querySelector(".display-screen");
 const valueButtons = document.querySelectorAll(".value");
 valueButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
-        let value = btn.textContent;
-        processInput(value);
-        displayValue(value);
+        if (!display.textContent.includes("ERROR")) { // prevent user from inputting any values if display is showing error (they have to click AC)
+            displayValue(btn.textContent);
+        }
     });
 });
 
 const equalsBtn = document.querySelector(".equals-btn");
 equalsBtn.addEventListener("click", () => {
-    display.textContent = "";
-    let answer = operate(operator, num1, num2);
-    if (Number(answer) === answer && answer % 1 !== 0) { // answer is floating point number
-        answer = +(answer.toFixed(4)); // round to 4 d.p. (if more than 4 d.p.)
-    }
-    displayValue(answer);
+    if (display.textContent != "") { // if user clicks "=" w/o inputting anything, do nothing
+        const equation = display.textContent;
+        setValues(equation); // sets num1, num2, operator
+        display.textContent = "";
+        if (operator === undefined) { // user only inputs first #
+            displayValue((num1.includes("-")) ? "Syntax ERROR" : num1); // if user uses (-) as minus
+        }
+        else if (num1 === "-") {
+            let ans;
+            if (operator === "+" || operator === "-") ans = operate("×", -1, (operator + num2));
+            else ans = "Syntax ERROR";
+            displayValue(ans);
+        }
+        else if (num1 === "" && (operator === "×" || operator === "÷")) {
+            displayValue("Syntax ERROR");
+        }
+        else if (num2 === "" || num2 === "-") { // user inputs first # and operator only || user only inputs (-) for second #
+            displayValue("Syntax ERROR");
+        }
+        else {
+            let answer = operate(operator, num1, num2);
+            if (Number(answer) === answer && answer % 1 !== 0) { // answer is floating point number
+                answer = +(answer.toFixed(4)); // round to 4 d.p. (if more than 4 d.p.)
+            }
+            displayValue(answer);
+        }
+    } 
 });
 
 const clearBtn = document.querySelector(".clear-btn");

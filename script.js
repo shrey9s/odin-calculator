ca0d1a2db1307506641536dd13aac37701c66428
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-24 19:21:25 +1300

    Implement getAnswer function
    The code inside the equals button event listener that was error-checking
    and evaluating the calculator operations was transferred and reworked into
    the getAnswer function, which replaces the processInput and setValue
    functions.

--- script.js
+++ script.js
@@ -47,9 +47,29 @@ function displayValue(val) {
     display.appendChild(textNode);
 }
 
-function setValues(eqn) {
+function getAnswer(eqn) {
+    let ans;
     const eqnParts = eqn.split(" ");
-    [num1, operator, num2] = [eqnParts[0], eqnParts[1], eqnParts[2]]
+    [num1, operator, num2] = [eqnParts[0], eqnParts[1], eqnParts[2]];
+    if (operator === undefined) { // user only inputs first #
+        ans = (num1.includes("-") && num1.lastIndexOf("-") !== 0) ? "Syntax ERROR" : num1; // if user uses (-) as minus -> Syntax ERROR
+    }
+    else if (num1 === "" && (operator === "×" || operator === "÷")) { // user only inputs operator (×/÷) and second #
+        ans = "Syntax ERROR";
+    }
+    else if (num1 === "-") { // user only inputs (-) for first #
+        ans = (operator === "+" || operator === "-") ? operate("×", -1, (operator + num2)) : "Syntax ERROR"; 
+    }
+    else if (num2 === "" || num2 === "-") { // user inputs first # and operator only || user only inputs (-) for second #
+        ans = "Syntax ERROR";
+    }
+    else {
+        ans = operate(operator, num1, num2);
+        if (Number(ans) === ans && ans % 1 !== 0) { // answer is floating point number
+            ans = +(ans.toFixed(4)); // round to 4 d.p. (if more than 4 d.p.)
+        }
+    }
+    return ans;
 }
 
 let num1, num2, operator;
@@ -59,7 +79,7 @@ const display = document.querySelector(".display-screen");
 const valueButtons = document.querySelectorAll(".value");
 valueButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
-        if (!display.textContent.includes("ERROR")) { // prevent user from inputting any values if display is showing error (they have to click AC)
+        if (!displayContent.includes("ERROR")) { // prevents user from inputting any values if display is showing error (they have to click AC)
             displayValue(btn.textContent);
         }
     });
@@ -69,31 +89,10 @@ const equalsBtn = document.querySelector(".equals-btn");
 equalsBtn.addEventListener("click", () => {
     if (display.textContent != "") { // if user clicks "=" w/o inputting anything, do nothing
         const equation = display.textContent;
-        setValues(equation); // sets num1, num2, operator
-        display.textContent = "";
-        if (operator === undefined) { // user only inputs first #
-            displayValue((num1.includes("-")) ? "Syntax ERROR" : num1); // if user uses (-) as minus
-        }
-        else if (num1 === "-") {
-            let ans;
-            if (operator === "+" || operator === "-") ans = operate("×", -1, (operator + num2));
-            else ans = "Syntax ERROR";
-            displayValue(ans);
-        }
-        else if (num1 === "" && (operator === "×" || operator === "÷")) {
-            displayValue("Syntax ERROR");
-        }
-        else if (num2 === "" || num2 === "-") { // user inputs first # and operator only || user only inputs (-) for second #
-            displayValue("Syntax ERROR");
-        }
-        else {
-            let answer = operate(operator, num1, num2);
-            if (Number(answer) === answer && answer % 1 !== 0) { // answer is floating point number
-                answer = +(answer.toFixed(4)); // round to 4 d.p. (if more than 4 d.p.)
-            }
-            displayValue(answer);
-        }
-    } 
+        const answer = getAnswer(equation); // sets num1, num2, operator and returns the answer
+        display.textContent = "";   
+        displayValue(answer);
+    }
 });
 
 const clearBtn = document.querySelector(".clear-btn");
